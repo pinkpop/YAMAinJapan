81e0befea0d2bd81c707238a14a75c2274d0be0e
pinkpop <dashiontang@gmail.com>
2022-08-17 22:15:14 +0900

    add cluster map to index

--- models/mountain.js
+++ models/mountain.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 const MountainSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -38,7 +40,13 @@ const MountainSchema = new Schema({
             ref: 'Review'
         }
     ]
-})
+},opts);
+
+MountainSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/mountains/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
 
 MountainSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
